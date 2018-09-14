c452e0dd8f81e0e2948ba15ae3d4f9ef12234d5f
Nguyen Vu Long <nguyenvulong2002@gmail.com>
2018-09-14 16:55:22 +0700

    Update multi select screen 3

--- test_react_1/src/ScreenThree.js
+++ test_react_1/src/ScreenThree.js
@@ -13,16 +13,28 @@ class ScreenThree extends Component {
         this.onDishCountChange = this.onDishCountChange.bind(this);
         this.getListFromData = this.getListFromData.bind(this);
         this.state = {
-            value : 1
+            selectedDishes:[
+                {
+                    name: "",
+                    count: 0
+                }
+            ]
         };
     }
 
 
-    onDishChange (selectedOption){
-        this.props.onDishChange(selectedOption.value);
-    }
+    onDishChange = name => event => {
+        console.log("Page change is onDishChange" + event );
+
+        let result = Object.entries(event);
+        result.map((item, index)=>{
+            console.log('key is:- ', item[0], ' and value is:- ', item[1]);
+        });
+        this.props.onDishChange(event.value);
+    };
 
     onDishCountChange (selectedOption){
+        console.log("Page change is onDishCountChange " + selectedOption.target.name);
         this.setState({ value: selectedOption.target.value  });
         // this.props.onDishChange(selectedOption.target.value);
     }
@@ -43,18 +55,37 @@ class ScreenThree extends Component {
         return listRes;
     }
 
+
+    createTable = () => {
+        let table = []
+
+        // Outer loop to create parent
+        for (let i = 0; i < 3; i++) {
+            let children = []
+            children.push(<Select
+                className="col-sm"
+                name  = {"Select" + i}
+                key  = {"Select" + i}
+                onChange={this.onDishChange(i)}
+                options={this.options}
+            />)
+            children.push(<input
+                type="number"
+                name  = {"input" + i }
+                key  = {"input" + i }
+                min={1} max={10}
+                value={this.state.selectedDishes[0].count}
+                onChange={this.onDishCountChange}/>)
+
+            table.push(<div className="row" key  = {"div" + i}>{children}</div>)
+        }
+        return table
+    }
+
     render(){
         return (
             <div style={{ width: 1000, height: 200}}>
-                <div className="row">
-                    <Select
-                        className="col-sm"
-                        onChange={this.onDishChange}
-                        options={this.options}
-                    />
-                    <input type="number" min={1} max={10} value={this.state.value} onChange={this.onDishCountChange}/>
-                </div>
-
+                {this.createTable()}
             </div>
         );
     }
